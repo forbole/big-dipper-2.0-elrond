6e2c3a5b8e9a83602af62a9129f26789378ed049
ryuash <ryuash123@gmail.com>
2022-01-18 10:36:31 +0800

    wip: follow up

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -6,17 +6,12 @@ import { useStyles } from './styles';
 import {
   DataBlocks,
 } from './components';
-import {
-  useDataBlocks,
-} from './hooks';
 
 const Home = () => {
   const classes = useStyles();
-  const { state: dataBlocksState } = useDataBlocks();
   return (
     <Layout className={classes.root}>
       <DataBlocks
-        {...dataBlocksState}
         className={classes.dataBlocks}
       />
     </Layout>

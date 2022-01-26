491a40f67c4134fb195c51aaa4befc028232887a
Ryuash <42913823+ryuash@users.noreply.github.com>
2022-01-26 13:50:12 +0800

    . (#14)

--- src/utils/go_to_page.ts
+++ src/utils/go_to_page.ts
@@ -3,3 +3,6 @@ export const BLOCKS = '/blocks';
 export const BLOCK_DETAILS = (hash: string) => `/blocks/${hash}`;
 export const MINIBLOCK_DETAILS = (hash: string) => `/miniblocks/${hash}`;
 export const TRANSACTIONS = '/transactions';
+export const TRANSACTION_DETAILS = (hash: string) => `/transactions/${hash}`;
+export const NODE_DETAILS = (hash: string) => `/nodes/${hash}`;
+export const ACCOUNT_DETAILS = (hash: string) => `/accounts/${hash}`;

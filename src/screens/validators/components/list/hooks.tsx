30ffa0b1d5c080eca10ebed773f6fe8981c67619
ryuash <ryuash123@gmail.com>
2022-01-27 15:53:11 +0800

    wip: setting up hook

--- src/screens/validators/components/list/hooks.tsx
+++ src/screens/validators/components/list/hooks.tsx
@@ -8,10 +8,13 @@ import {
 } from '@api';
 import { formatToken } from '@utils/format_token';
 import { chainConfig } from '@configs';
-import { ValidatorsState } from './types';
+import {
+  ValidatorsState, ValidatorType,
+} from './types';
 import { fakeData } from './fakes';
 
 export const useValidators = () => {
+  const [search, setSearch] = useState('');
   const [state, setState] = useState<ValidatorsState>({
     loading: true,
     exists: true,
@@ -27,6 +30,76 @@ export const useValidators = () => {
 
   const handleSetState = (stateChange: any) => {
     setState((prevState) => R.mergeDeepLeft(stateChange, prevState));
+  };
+
+  const handleTabChange = (_event: any, newValue: number) => {
+    setState((prevState) => ({
+      ...prevState,
+      tab: newValue,
+    }));
+  };
+
+  const handleSort = (key: string) => {
+    if (key === state.sortKey) {
+      setState((prevState) => ({
+        ...prevState,
+        sortDirection: prevState.sortDirection === 'asc' ? 'desc' : 'asc',
+      }));
+    } else {
+      setState((prevState) => ({
+        ...prevState,
+        sortKey: key,
+        sortDirection: 'asc', // new key so we start the sort by asc
+      }));
+    }
+  };
+
+  const sortItems = (items: ValidatorType[]) => {
+    let sorted: ValidatorType[] = R.clone(items);
+
+    if (state.tab === 0) {
+      // sorted = sorted.filter((x) => x.status === 3);
+    }
+
+    if (state.tab === 1) {
+      // sorted = sorted.filter((x) => x.status !== 3);
+    }
+
+    if (search) {
+      sorted = sorted.filter((x) => {
+        const formattedSearch = search.toLowerCase().replace(/ /g, '');
+        return (
+          x.name.toLowerCase().replace(/ /g, '').includes(formattedSearch)
+          || x.identity.toLowerCase().includes(formattedSearch)
+        );
+      });
+    }
+
+    if (state.sortKey && state.sortDirection) {
+      sorted.sort((a, b) => {
+        let compareA = R.pathOr(undefined, [...state.sortKey.split('.')], a);
+        let compareB = R.pathOr(undefined, [...state.sortKey.split('.')], b);
+
+        if (typeof compareA === 'string') {
+          compareA = compareA.toLowerCase();
+          compareB = compareB.toLowerCase();
+        }
+
+        if (compareA < compareB) {
+          return state.sortDirection === 'asc' ? -1 : 1;
+        }
+        if (compareA > compareB) {
+          return state.sortDirection === 'asc' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+
+    return sorted;
+  };
+
+  const handleSearch = (value: string) => {
+    setSearch(value);
   };
 
   const getValidators = async () => {
@@ -64,5 +137,9 @@ export const useValidators = () => {
 
   return ({
     state,
+    handleTabChange,
+    handleSort,
+    handleSearch,
+    sortItems,
   });
 };

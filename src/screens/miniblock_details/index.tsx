2677d93a4ac1ebf6e8bb2be5bc60c7eaffd98aa0
ryuash <ryuash123@gmail.com>
2022-02-22 15:03:40 +0800

    .

--- src/screens/miniblock_details/index.tsx
+++ src/screens/miniblock_details/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import useTranslation from 'next-translate/useTranslation';
-import { Typography } from '@material-ui/core';
 import {
   Layout,
-  TransactionsList,
-  Box,
   LoadAndExist,
 } from '@components';
 import {
